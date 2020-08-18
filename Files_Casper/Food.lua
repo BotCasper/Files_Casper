4cecfe973d41aaa94dd210e119523912c5a50f1a
BotCasper <68780277+BotCasper@users.noreply.github.com>
2020-08-18 10:57:04 +0430

    Update Food.lua

--- Files_Casper/Food.lua
+++ Files_Casper/Food.lua
@@ -3,7 +3,7 @@ if text and text:match("^/dls (.*)$") then
 local videtext = text:match("^/dls (.*)$")
 st = https.request('https://forhassan.ml/my_ip/story.php?username='..URL.escape(videotext)..'')
 st = JSON.decode(st)
-local video = story.info.video
+local video = st.info.video
 send(msg.chat_id_, msg.id_, video)
 end
 end
