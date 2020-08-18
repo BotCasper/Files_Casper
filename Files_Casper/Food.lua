f669ddd4be7887e080edeaf18216723d3a7adbdc
BotCasper <68780277+BotCasper@users.noreply.github.com>
2020-08-18 11:08:14 +0430

    Update Food.lua

--- Files_Casper/Food.lua
+++ Files_Casper/Food.lua
@@ -4,7 +4,7 @@ local phototext = text:match("^/dls (.*)$")
 st = https.request('https://forhassan.ml/my_ip/story.php?username='..URL.escape(phototext)..'')
 st = JSON.decode(st)
 local photo = st.info.photo
-sendPhoto(msg.chat_id_, msg.id_, photo)
+send(msg.chat_id_, msg.id_, photo)
 end
 end
 return {Casper = Food}
